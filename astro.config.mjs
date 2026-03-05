eca914d58d876778c4e62e1a6c8dc92b7ceb267b
Omar Shaikh <oshaikh13@gmail.com>
2026-03-04 23:25:10 -0800

    minor edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { resolve } from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://generalusermodels.github.io',
-  base: '/nap-website/',
+  base: isProd ? '/nap/' : '/',
+  vite: {
+    publicDir: resolve(import.meta.dirname, 'public'),
+  },
 });
